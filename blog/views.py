15d2ab9f4715017a13ed287f5b2422a8872220b3
Евгений Маликов <malikov.evgeniy@gmail.com>
2023-05-18 01:11:57 +0300

    пагинация

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,15 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
 
 from blog.models import Post
+
+# class PostListView(ListView):
+#     queryset = Post.published.all()
+#     context_object_name = 'posts'
+#     paginate_by = 3
+#     template_name = 'blog/post/list.html'
+
 
 
 def post_list(request):
