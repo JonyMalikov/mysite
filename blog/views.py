c5cc8f2cd5af725d551c1320b1aac276f465c3d8
Евгений Маликов <malikov.evgeniy@gmail.com>
2023-05-18 00:22:44 +0300

    пагинация

--- blog/views.py
+++ blog/views.py
@@ -1,19 +1,34 @@
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
 
 from blog.models import Post
 
 
 def post_list(request):
-    posts = Post.published.all()
+    post_list = Post.published.all()
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts=paginator.page(paginator.num_pages)
+
     return render(request,
                   'blog/post/list.html',
                   {'posts': posts})
 
 
-def post_detail(request, id):
-    post = get_object_or_404(Post,
-                             id=id,
-                             status=Post.Status.PUBLISHED)
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(
+        Post,
+        status=Post.Status.PUBLISHED,
+        slug=post,
+        publish__year=year,
+        publish__month=month,
+        publish__day=day
+    )
 
     return render(request,
                   'blog/post/detail.html',
