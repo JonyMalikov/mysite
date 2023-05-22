2fe0e53a5e46139badcaef50acc89039ec239669
Евгений Маликов <malikov.evgeniy@gmail.com>
2023-05-22 23:41:43 +0300

    Выделение основ слов и ранжирование результатов

--- blog/views.py
+++ blog/views.py
@@ -120,8 +120,8 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
-            search_vector = SearchVector('title', 'body')
-            search_query = SearchQuery(query)
+            search_vector = SearchVector('title', 'body', config='russian')
+            search_query = SearchQuery(query, config='russian')
             results = Post.published.annotate(
                 search=search_vector,
                 rank=SearchRank(search_vector, search_query)
