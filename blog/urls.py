127dbeb1d74f7d24a85c91d7c64f089c773d8d52
Евгений Маликов <malikov.evgeniy@gmail.com>
2023-05-22 15:30:36 +0300

    новостных лент для постов блога

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
+
+from .feeds import LatestPostsFeed
 from . import views
 
 app_name = 'blog'
 
 urlpatterns = [
-    # Post views
     path('', views.post_list, name='post_list'),
     # path('', views.PostListView.as_view(), name='post_list'),
     path('tag/<slug:tag_slug>/',
@@ -16,6 +17,5 @@ urlpatterns = [
          views.post_share, name='post_share'),
     path('<int:post_id>/comment/',
          views.post_comment, name='post_comment'),
-    # path('feed/', LatestPostsFeed(), name='post_feed'),
-    # path('search/', views.post_search, name='post_search'),
+    path('feed/', LatestPostsFeed(), name='post_feed'),
 ]
